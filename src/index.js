753ae95f9ab5f52ff6a8c4383d680772c0d99417
Victor Avdeev <avdeevvictor95@gmail.com>
2020-10-03 17:04:20 +0300

    feat: add min, max, avg functions for massives

--- src/index.js
+++ src/index.js
@@ -1,12 +1,40 @@
 
 exports.min = function min (array) {
-  return 0;
+  var result = 0;
+    if (array !== undefined){
+      if(array.length !== 0){
+        result = array[0];
+        for(var i = 1; i< array.length; i++){
+          result = (array[i] < result) ? array[i] : result;
+        }
+      }
+    }
+    return result;
 }
 
 exports.max = function max (array) {
-  return 0;
+    var result = 0;
+    if (array !== undefined){
+      if(array.length !== 0){
+        result = array[0];
+        for(var i = 1; i< array.length; i++){
+          result = (array[i] > result) ? array[i] : result;
+        }
+      }
+    }
+    return result;
 }
 
+
 exports.avg = function avg (array) {
-  return 0;
+    var result = 0;
+    if (array !== undefined){
+      if(array.length !== 0){
+        for(var i = 0; i< array.length; i++){
+          result += array[i];
+        }
+        result = result / array.length;
+      }
+    }
+    return result;
 }
